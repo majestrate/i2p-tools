e1979d7e5f316e874ac8735c4942e7c01d6cb4b0
jeff <ampernand@gmail.com>
2014-05-07 09:06:13 -0400

    rewrite netdb.py

--- netdb/src/netdb.py
+++ netdb/src/netdb.py
@@ -1,164 +1,194 @@
 ##
-## Thanks to psi for this python netdb reader!
-## Some modefications by Meeh
+## Python Netdb Reader
 ##
-import os,sys,struct,time,hashlib,fnmatch
+import os,sys,struct,time,hashlib,fnmatch,io
+import logging
 
-
-class _mutnum:
+def sha256(data,raw=False):
     """
-    Mutable Number
-    By Default python numbers are immutable
+    compute sha256 of data
+    """
+    h = hashlib.new('sha256')
+    h.update(data)
+    if raw:
+        return h.digest()
+    else:
+        return h.hexdigest()
+
+class Inspector:
+
+    _log = logging.getLogger('NDBInspector')
+    
+    def inspect(self, entry):
+        pass
+        
+    def run(self, ndb):
+        entry_counter = 0
+        for root, dirnames, filenames in os.walk(ndb):
+            for filename in fnmatch.filter(filenames, '*.dat'):
+                fname = os.path.join(root, filename)
+                e = Entry(fname)
+                e.verify()
+                if e.valid:
+                    entry_counter += 1
+                    self.inspect(e)
+                else:
+                    self._log.warn('invalid entry in file {}'.format(fname))
+        self._log.info('read {} entries'.format(entry_counter))
+
+class Address:
+    """
+    netdb address
     """
 
-    def __init__(self,val=0):
-        self.val = val
 
-    def inc(self,n):
-        self.val += n
-        return self
+class Entry:
+    """
+    netdb entry
+    """
+    _pubkey_size = 256
+    _signkey_size = 128
 
-    def dec(self,n):
-        self.val -= n
-        return self
+    _log = logging.getLogger('NDBEntry')
+
+    @staticmethod
+    def _read_short(fd):
+        Entry._log.debug('read_short')
+        return struct.unpack('!H',fd.read(2))[0]
+
+    @staticmethod
+    def _read_mapping(fd):
+        Entry._log.debug('read_mapping')
+        mapping = dict()
+        tsize = Entry._read_short(fd)
+        data = Entry._read(fd, tsize)
+        sfd = io.BytesIO(data)
+        ind = 0
+        while ind < tsize:
+            Entry._log.debug(ind)
+            key = Entry._read_string(sfd)
+            Entry._log.debug([key])
     
-    def less(self,n):
-        return self - n
+            ind += len(key) + 2
+            Entry._read_byte(sfd)
+            val = Entry._read_string(sfd)
+            Entry._log.debug([val])
 
-    def __len__(self):
-        return 0
-
-    def __str__(self):
-        return self.val.__str__()
+            ind += len(val) + 2
+            Entry._read_byte(sfd)
     
-    def __add__(self,x):
-        return self.val.__add__(x)
-
-    def __sub__(self,x):
-        return self.val.__sub__(x)
-
-    def __mul__(self,x):
-        return self.val.__mul__(x)
-
-    def __div__(self,x):
-        return self.val.__div__(x)
-
-    def __int__(self):
-        return self.val.__int__()
-
-    def __long__(self):
-        return self.val.__long__()
-    
-    def __float__(self):
-        return self.val.__float__()
-
-    def __oct__(self):
-        return self.val.__oct__()
-    
-    def __hex__(self):
-        return self.val.__hex__()
-    
-    def __index__(self):
-        return self.val.__index__()
-
-
-
-# begin lambda hacks
-_getslice = lambda d,l,h: d[l:h]
-_getint = lambda d: struct.unpack('L',d)[0]
-_getshort = lambda d: struct.unpack('!H',d)[0]
-_get = lambda d, i: d[i]
-if sys.version.startswith('3'):
-    _get = lambda d, i: chr(d[i])
-
-_getbyte = lambda d: d
-_getbyteasint = lambda d: struct.unpack('!B',d)[0]
-_getqlong = lambda d: struct.unpack('!Q',d)[0]
-_getnext = lambda d, n, i: _getslice(d,i.val,i.inc(n))
-_getrest = lambda d, i: d[i:]
-_getnextlong = lambda d, i: _getint(_getnext(d,8,i))
-_getnextbyte = lambda d, i: _getbyte(_getnext(d,1,i))
-_getnextbyteasint = lambda d, i: _getbyteasint(_getnext(d,1,i))
-_getnextshort = lambda d, i: _getshort(_getnext(d,2,i))
-_getnextstr = lambda d, i: _getnext(d,_getnextbyteasint(d,i),i)
-_mktime = lambda li: (time.asctime(time.gmtime(li)),li)
-_getnexttime = lambda d,i: _mktime(_getqlong(_getnext(d,8,i))/1000.0)
-# end lambda hacks
-
-
-def _put_next_mapping(mapping,ind,data):
-    tsize = _getnextshort(data,ind)
-    data = _getnext(data,tsize,ind)
-    ind = _mutnum()
-    while ind.val < tsize:
-        key = _getnextstr(data,ind)
-        assert ord(_get(data,ind)) == ord(b'=')
-        ind.inc(1)
-        val = _getnextstr(data,ind)
-        assert ord(_get(data,ind)) == ord(b';')
-        ind.inc(1)
-        if key in mapping:
-            v = mapping[key]
-            if isinstance(v,list):
-                mapping[key].append(val)
+            key = key[:-1]
+            val = val[:-1]
+            if key in mapping:
+                v = mapping[key]
+                if isinstance(v,list):
+                    mapping[key].append(val)
+                else:
+                    mapping[key] = [v,val]
             else:
-                mapping[key] = [v,val]
-        else:
-            mapping[key] = val
+                mapping[key] = val
+        return mapping
 
+    @staticmethod
+    def _read(fd, amount):
+        Entry._log.debug('read %d bytes' % amount)
+        return fd.read(amount)
 
-def _read_rid(rid,data,pksize=256,sksize=128):
-    ind = _mutnum()
-    rid['pkey'] = hashlib.sha256(_getnext(data,pksize,ind)).hexdigest()
-    rid['skey'] = hashlib.sha256(_getnext(data,sksize,ind)).hexdigest()
-    rid['cert'] = hashlib.sha256(_getrest(data,ind)).hexdigest()
-
-def _read_addr(addr,ind,data):
-    addr['cost'] = _getnextbyteasint(data,ind)
-    addr['expire'] = _getnexttime(data,ind)
-    addr['transport'] = _getnextstr(data,ind)
-    opts = dict()
-    _put_next_mapping(opts,ind,data)
-    addr['options'] = opts
-
-def _read_entry(entry,fname):
-    """
-    read a netdb entry from a file
-    to an object that can be accessed like a dictionary
-    """
-
-    try: # read data
-        with open(fname,'rb') as r:
-            data = r.read()
-    except:
-        return False
-
-    ind = _mutnum()
-    rid = dict()
-    _read_rid(rid,_getnext(data,387,ind))
-    entry['router_id'] = rid
-    entry['published'] = _getnexttime(data,ind)
-    size = _getnextbyteasint(data,ind)
-    entry['addrs'] = []
-    for n in range(size):
-        addr = dict()
-        _read_addr(addr,ind,data)
-        entry['addrs'].append(addr)
-
-    entry['peer_size'] = _getnextbyteasint(data,ind)
-    opts = dict()
-    _put_next_mapping(opts,ind,data)
-    entry['options'] = opts
-    entry['signature'] = hashlib.sha256(_getnext(data,40,ind)).hexdigest()
-    entry['routerHash'] = fname.split('routerInfo-')[-1].split('.dat')[0]
-    return True
-
-
-def _print_hook(netdbe):
-    print (netdbe)
+    @staticmethod 
+    def _read_byte(fd):
+        return struct.unpack('!B', Entry._read(fd,1))[0]
     
+    @staticmethod
+    def _read_string(fd):
+        Entry._log.debug('read_string')
+        slen = Entry._read_byte(fd)
+        return Entry._read(fd, slen)
 
-def inspect(hook=_print_hook,netdb_dir=os.path.join(os.environ['HOME'],'.i2p','netDb')):
+    @staticmethod
+    def _read_time(fd):
+        li = struct.unpack('!Q', fd.read(8))
+        return li
+
+    @staticmethod
+    def _read_addr(fd):
+        """
+        load next router address
+        """
+        Entry._log.debug('read_addr')
+        addr = Address()
+        addr.cost = Entry._read_byte(fd)
+        addr.expire = Entry._read_time(fd)
+        addr.transport = Entry._read_string(fd)
+        addr.options = Entry._read_mapping(fd)
+
+
+
+    def __init__(self, filename):
+        """
+        construct a NetDB Entry from a file
+        """
+        self.addrs = list()
+        self.options = dict()
+        self.pubkey = None
+        self.signkey = None
+        self.cert = None
+        self.published = None
+        self.signature = None
+        self.peer_size = None
+        self.valid = False
+        with open(filename, 'rb') as fr:
+            self._load(fr)
+            self.routerHash = filename.split('routerInfo-')[-1].split('.dat')[0]
+            
+    def _load(self, fd):
+        """
+        load from file descriptor
+        """
+        data = self._read(fd, 387)
+        ind = 0
+        # public key
+        self.pubkey = sha256(data[ind:ind+self._pubkey_size])
+        ind += self._pubkey_size
+
+        # signing key
+        self.signkey = sha256(data[ind:ind+self._signkey_size])
+        ind + self._signkey_size
+
+        # certificate
+        self.cert =  sha256(data[ind:])
+
+        # date published
+        self.published  = self._read_time(fd)
+        
+        # reachable addresses
+        self.addrs = list()
+        addrlen = self._read_byte(fd)
+        for n in range(addrlen):
+            addr = self._read_addr(fd)
+            self.addrs.append(addr)
+ 
+        # peer size
+        self.peer_size = self._read_byte(fd)
+        
+        # other options
+        self.options = self._read_mapping(fd)
+        # signature
+        self.signature = sha256(self._read(fd, 40))
+
+    def verify(self):
+        """
+        verify router identity
+        """
+        #TODO: verify 
+        self.valid = True
+
+    def dict(self):
+        """
+        return dictionary in old format
+        """
+        #TODO: implement old format
+
+def inspect(hook=None,netdb_dir=os.path.join(os.environ['HOME'],'.i2p','netDb')):
     """
     iterate through the netdb
     
@@ -172,20 +202,14 @@ def inspect(hook=_print_hook,netdb_dir=os.path.join(os.environ['HOME'],'.i2p','n
       netdb_dir - path to netdb folder
         - defaults to $HOME/.i2p/netDb/
 
-    """
-    for root, dirnames, filenames in os.walk(netdb_dir):
-        for filename in fnmatch.filter(filenames, '*.dat'):
-            fname = os.path.join(root, filename)
-            e = dict()
-            # Added a try. If one netDb file is corrupted, all fails if no try is used.
-            try:
-                if _read_entry(e,fname):
-                    r = hook(e)
-                    if r is not None:
-                        print r
-                        return r
-            except:
-                pass
+    """ 
+
+    insp = Inspector()
+    if hook is not None:
+        insp.inspect = hook 
+    insp.run(netdb_dir)
 
 
-
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    inspect()
