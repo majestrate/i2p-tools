3f4d7a14c9e6d4ef5959c1eb35bd40f4921f83e1
jeff <ampernand@gmail.com>
2015-01-14 21:35:08 -0500

    make i2tun use new python-i2cp api

--- i2tun/i2tun.py
+++ i2tun/i2tun.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3.4
 from i2p.i2cp import client as i2cp
+import trollius as asyncio
 import pytun
 import threading
 import logging
@@ -14,28 +15,32 @@ class IPV4Handler(i2cp.I2CPHandler):
         self._iface.addr = our_addr
         self._iface.dstaddr = their_addr
         self._iface.mtu = mtu
+        self._mtu = mtu
         self._iface.up()
 
+    @asyncio.coroutine
     def session_made(self, con):
         print ('we are {}'.format(con.dest.base32()))
         self.con = con
-        threading.Thread(target=self.mainloop, args=(con,)).start()
+        asyncio.get_event_loop().add_reader(self._iface, self._read_packet)
 
-    def mainloop(self, con):
-        while True:
-            buff = self._iface.read(self._iface.mtu)
-            self.con.send_dsa_dgram(self._them, buff)
+    def _read_packet(self):
+        data = self._iface.read(self._mtu)
+        self.con.send_dsa_dgram(self._them, data)
 
+    @asyncio.coroutine
     def got_dgram(self, dest, data, srcport, dstport):
         if dest.base32() == self._them:
             self._iface.write(data)
-        
+
+    def __del__(self):
+        del self._iface
 
 def main():
     import argparse
     import logging
     ap = argparse.ArgumentParser()
-    logging.basicConfig()
+    #logging.basicConfig(level=logging.DEBUG)
     ap.add_argument('--remote', required=True, type=str)
     ap.add_argument('--our-addr', required=True, type=str)
     ap.add_argument('--their-addr', required=True, type=str)
@@ -46,11 +51,13 @@ def main():
     args = ap.parse_args()
 
     handler = IPV4Handler(args.remote, args.our_addr, args.their_addr, args.mtu)
-    con = i2cp.Connection(handler, i2cp_host=args.i2cp_host, i2cp_port=args.i2cp_port, session_options={'inbound.quantity' : '2', 'outbound.quantity' : '2', 
-                                                                                                        'i2cp.dontPublishLeaseSet' : 'false' })
+    con = i2cp.Connection(handler, i2cp_host=args.i2cp_host, i2cp_port=args.i2cp_port, session_options={'inbound.quantity' : '2', 'outbound.quantity' : '2'})
     con.open()
-    con.start()
-
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_forever()
+    finally:
+        loop.close()
 
 if __name__ == '__main__':
     main()
