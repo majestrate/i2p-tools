f5aa9ffa913df447d7b1440ac006bffc6dfabc38
Chris Barry <chris@barry.im>
2015-04-26 17:51:28 -0400

    Adding geoip data to Address class

--- netdb/setup.py
+++ netdb/setup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
-from distutils.core import setup
+from setuptools import setup
 
 setup(name = 'netdb',
     version = '0.0',
     description = 'i2p netdb parser',
     author = 'Jeff Becker',
     author_email = 'ampernand@gmail.com',
+	install_requires = ['python-geoip','python-geoip-geolite2'],
     url = 'https://github.com/majestrate/i2p-tools',
     package_dir = {'netdb': 'src'},
     packages = ['netdb'],
