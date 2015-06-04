d6708ff0835c781195e481b139f589490d122ae9
Chris Barry <chris@barry.im>
2015-06-04 11:00:47 -0400

    Cleaning up some minor bugs

--- i2pcontrol/setup.py
+++ i2pcontrol/setup.py
@@ -6,7 +6,7 @@ setup(name = 'i2pcontrol',
     description = 'i2pcontrol wrapper',
     author = 'Chris Barry',
     author_email = 'chris@barry.im',
-	classifiers = 'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)'
+	classifiers = 'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
     #install_requires = 'python-jsonrpc>=0.7.4',
     url = 'https://github.com/majestrate/i2p-tools',
     package_dir = {'i2pcontrol': 'i2pcontrol'},
