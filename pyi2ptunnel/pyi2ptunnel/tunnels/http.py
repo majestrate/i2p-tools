48cc06575b646321673200642d59c47f56174bd2
jeff <ampernand@gmail.com>
2015-09-08 19:33:54 -0400

    invalid package name

--- pyi2ptunnel/pyi2ptunnel/tunnels/http.py
+++ pyi2ptunnel/pyi2ptunnel/tunnels/http.py
@@ -3,7 +3,7 @@ from twisted.python.compat import urllib_parse, urlquote
 from twisted.internet.protocol import ClientFactory
 from twisted.internet.endpoints import connectProtocol
 from twisted.web.http import HTTPFactory, HTTPClient, HTTPChannel, Request
-from twisted.web.http.proxy import ProxyClient, ProxyClientFactory
+from twisted.web import ProxyClient, ProxyClientFactory
 from pyi2ptunnel import util
 
 
