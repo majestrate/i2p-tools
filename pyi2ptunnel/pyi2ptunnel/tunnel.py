ca7e79022d58f7e10486d795d12f6bdc0f18667a
jeff <ampernand@gmail.com>
2015-09-09 08:36:42 -0400

    ugh

--- pyi2ptunnel/pyi2ptunnel/tunnel.py
+++ pyi2ptunnel/pyi2ptunnel/tunnel.py
@@ -25,7 +25,7 @@ class TunnelFactory(object):
 
     def endpoint(self, addr, port): 
         if addr.endswith('.i2p'):
-            ep = 'i2p:{}'.format(addr)
+            ep = 'i2p:{}:api={}'.format(addr, self.api)
         else:
             ep = 'tor:host={}:port={}:socksPort={}'.format(addr, port, socksPort)
         print (ep)
