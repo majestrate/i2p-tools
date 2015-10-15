7989d0e5498a4dbd7a8c915d471bf18b6713c83c
jeff <ampernand@gmail.com>
2015-10-15 15:38:46 -0400

    correct index

--- samtun/samtun/link.go
+++ samtun/samtun/link.go
@@ -62,7 +62,7 @@ func (f linkFrame) Bytes() []byte {
 func frameFromBytes(buff []byte) (f linkFrame) {
   if buff[0] == 0x00 {
     pkts := buff[1]
-    idx := 2
+    idx := 0
     for pkts > 0 {
       plen := binary.BigEndian.Uint16(buff[:idx])
       if 2 + int(plen) + idx < len(buff) {
