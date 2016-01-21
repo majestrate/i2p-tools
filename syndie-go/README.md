60a873197275fd13955d5b88da9db1c9c66def56
Jeff Becker <ampernand@gmail.com>
2016-01-21 10:30:24 -0500

    initial stubs for syndie-go

---
+++ syndie-go/README.md
@@ -0,0 +1,5 @@
+## syndie-go
+
+[Syndie](https://syndie.i2p2.de/) implementation in go, (WIP)
+
+    go get -u github.com/majestrate/i2p-tools/syndie-go/cmd/syndie
---
+++ syndie-go/cmd/syndie/syndie.go
@@ -0,0 +1,13 @@
+/**
+  entry point for syndie distributed forums
+*/
+package main
+
+import (
+  "log"
+)
+
+func main() {
+  log.Println("this program does nothing ring now")
+}
+
