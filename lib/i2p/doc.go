66608e4f41b5909977ac67e9151f9905c1e6eb52
Jeff Becker <ampernand@gmail.com>
2016-03-06 07:18:55 -0500

    update docs for i2p library

--- lib/i2p/doc.go
+++ lib/i2p/doc.go
@@ -1,7 +1,7 @@
 /*
 library for connecting go applications into i2p with relative ease
 
-implements `net.Listener`, `net.Conn`, `net.Addr`, `net.Dial` that goes over i2p
+implements net.Listener, net.Conn, net.Addr, net.Dial, net.PacketConn that goes over i2p
 
     package main
 
@@ -13,14 +13,14 @@ implements `net.Listener`, `net.Conn`, `net.Addr`, `net.Dial` that goes over i2p
       "path/filepath"
     )
 
-    // see i2p.Session interface for more usage
+    // see i2p.StreamSession interface for more usage
 
     func main() {
        var err error
-       var sess i2p.Session
+       var sess i2p.StreamSession
        // connect to an i2p router
        // you can pass in "" to generate a transient session that doesn't save the destination keys
-       sess, err = i2p.NewSessionEasy("127.0.0.1:7656", filepath.Join("some", "path", "to", "privatekey.txt"))
+       sess, err = i2p.NewStreamSessionEasy("127.0.0.1:7656", filepath.Join("some", "path", "to", "privatekey.txt"))
        if err != nil {
          fmt.Printf("failed to open connection to i2p router: %s", err.Error())
          return
