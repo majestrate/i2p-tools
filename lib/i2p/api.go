078e48ea2e238a29c0d9354e3bcc3c0e71931e0e
Jeff Becker <ampernand@gmail.com>
2016-06-16 10:22:01 -0400

    update lib/i2p

--- lib/i2p/api.go
+++ lib/i2p/api.go
@@ -4,31 +4,34 @@ import (
   "net"
 )
 
-//
-// a session with the i2p router
-//
+// a session with an i2p router
 type Session interface {
-  // get base32 address string for this session
-  B32() string
-	// lookup the address of a name on i2p
-  Lookup(name string) (net.Addr, error)
+	// get printable b32 address of this session
+	B32() string
+	// ensure a keyfile exists
+	EnsureKeyfile(fpath string) (err error)
 }
 
-
+//
+// session for accepting and creating reliable stream oriented connections over i2p from 1 anonymous endpoint
+//
 type StreamSession interface {
-	// implements session
+	// implements i2p.Session
 	Session
 	// implements net.Listener
 	net.Listener
   // Dial out to i2p from this session
   // same api as net.Dialer.Dial
+	// does not dial out to anything except i2p addresses
+	// will fail if attempting to connect to something not accessable via the i2p network layer
   Dial(network, addr string) (net.Conn, error)
 }
 
-type I2PDestHash [32]byte
-
+//
+// session for sending and receiving large semi-reliable messages over i2p from 1 anonymous endpoint
+//
 type PacketSession interface {
-	// implements session
+	// implements i2p.Session
 	Session
 	// implements net.PacketConn
 	net.PacketConn
